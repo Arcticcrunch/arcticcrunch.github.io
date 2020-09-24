ca2ab377a67a559c87e2b6d2505e7627014d4161
Arcticcrunch <45840800+Arcticcrunch@users.noreply.github.com>
2020-09-24 22:50:25 +0300

    upd

--- Scripts.js
+++ Scripts.js
@@ -1,15 +1,6 @@
-"use strict"
 
 
 function NotImplemented()
 {
     alert("Функционал этой кнопки ещё не реализован :)");
 }
-
-// Прокрутка страницы в самый верх
-function ScrollToTop()
-{
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
-
